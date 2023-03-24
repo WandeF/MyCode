f5ab0add703f2c6e613e1de32569eae1fd480686
WandeF <2665233825@qq.com>
2023-03-24 11:39:59 +0800

    Update AgetoDays.cpp

--- C/code/AgetoDays/AgetoDays.cpp
+++ C/code/AgetoDays/AgetoDays.cpp
@@ -1,14 +1,18 @@
+/*
+	给出生日期计算活了多少天
+	Calculate the number of days lived for a date of birth
+*/
 #include <stdio.h>
 #include <time.h>
 
-int countDay(int birthyear, int birthmon, int birthday);
-int isLeapYear(int year);
-int dayOfMonth(int year, int month);
+int countDay(int birthyear, int birthmon, int birthday);//计算天数函数 The function to count days
+int isLeapYear(int year); //isLeapYear
+int dayOfMonth(int year, int month); //给定年份和月份计算当月天数 give me year and month to count days of month
 
 int main()
 {
-	int birthyear, birthmon, birthday;
-	int days;
+	int birthyear, birthmon, birthday; // 出生年月日 birthday
+	int days; //your days 
 
 	printf("please input your birthday: \n");
 
@@ -16,13 +20,13 @@ int main()
 
 	days = countDay(birthyear, birthmon, birthday);
 
-	printf("your days till your birth are %d", days);
+	printf("your days till your birth are %d .", days);
 }
 int countDay(int birthyear, int birthmon, int birthday)
 {
-	int toyear, tomon, today, yday, bday;
-	int countday = 0;
-	int subday = 0;
+	int toyear, tomon, today, yday, bday; // 用gmtime函数给出当前年月日   yday：从今年1月1日起过了多少天    bday：从出生年份1月1日起过了多少天
+ 	int countday = 0;
+	int subday = 0; // yday + 出生年天数(365/366)-bday+1
 
 	
 	time_t timep;
@@ -30,12 +34,12 @@ int countDay(int birthyear, int birthmon, int birthday)
 	struct tm *p;
 	p = gmtime(&timep);
 
-	toyear = 1900+p->tm_year;
+	toyear = 1900+p->tm_year; //从1900到现在有多少年，计算年份需+1900
 	tomon = p->tm_mon;
 	today = p->tm_mday;
 	yday = p->tm_yday;
 
-	for (int i=birthyear+1; birthyear < i && i < toyear; i++)
+	for (int i=birthyear+1; birthyear < i && i < toyear; i++) //中间年份的日期(完整年的天数之和)
 	{
 		if (isLeapYear(i)== 1)
 		{
@@ -47,11 +51,12 @@ int countDay(int birthyear, int birthmon, int birthday)
 		}
 	}
 
-	for (int j = 1; j < birthmon; j++) {
+	for (int j = 1; j < birthmon; j++) // 出生年份1月1到出生月的天数之和(完整月的天数和) 
+	{
 		bday += dayOfMonth(birthyear, j);
 	}
 
-	bday += birthday;
+	bday += birthday; //从1月1到出生日的天数和
 
 	subday = 365 + isLeapYear(birthyear) - bday + yday+1;
 
