3ccf310ffd0477c60e65aec29386c35db6e0a6b3
WandeF <2665233825@qq.com>
2023-05-25 10:11:05 +0800

    Update smtpServer.py

--- Python/webMail/smtpServer.py
+++ Python/webMail/smtpServer.py
@@ -6,12 +6,14 @@ smtp_server = "smtp.qq.com"
 smtp_port = 25
 
 sender = "2665233825@qq.com"
-password = "lcgzbfinkekeeacb"
+password = ""
 
 def send_mail(smtp_socket, sender, receiver, title, content):
 
+    #将消息整理成符合smtp格式的数据，发送出去
     data = ""
     
+    #整合
     data = "From: " + sender + "\r\n"
     data += "To: " + receiver + "\r\n"
     data += "Subject: " + title + "\r\n"
@@ -19,7 +21,7 @@ def send_mail(smtp_socket, sender, receiver, title, content):
     data += "\r\n"  # 空行分隔头部和正文
     data += content
 
-
+    #发送sender邮箱
     smtp_socket.send(("MAIL FROM: <" + sender + ">\r\n").encode())
     recv0 = smtp_socket.recv(1024).decode()
     print("c:"+"MAIL FROM: <" + sender + ">\r\n")
@@ -27,7 +29,7 @@ def send_mail(smtp_socket, sender, receiver, title, content):
         print('250 reply not received from server')
     print("s:"+recv0)
 
-
+    #发送receiver邮箱
     smtp_socket.send(("RCPT TO: <" + receiver + ">\r\n").encode())
     recv1 = smtp_socket.recv(1024).decode()
     print("c:"+"RCPT TO: <" + receiver + ">\r\n")
@@ -35,7 +37,7 @@ def send_mail(smtp_socket, sender, receiver, title, content):
         print('250 reply not received from server')
     print("s:"+recv1)
 
-
+    #发送整合好的数据
     smtp_socket.send("DATA\r\n".encode())
     recv2 = smtp_socket.recv(1024).decode()
     print("c:"+"DATA\r\n")
@@ -43,7 +45,7 @@ def send_mail(smtp_socket, sender, receiver, title, content):
         print('354 reply not received from server')
     print("s:"+recv2)
     
-
+    #数据以.结尾
     smtp_socket.send(data.encode() + b"\r\n.\r\n")
     recv3 = smtp_socket.recv(1024).decode()
     print("c:"+data)
@@ -54,6 +56,7 @@ def send_mail(smtp_socket, sender, receiver, title, content):
 
 def start_smtp(receiver, title, content):
 
+    #新建一个socket对象，用于与smtp服务器之间的连接
     smtp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     smtp_socket.connect((smtp_server, smtp_port))
     recv = smtp_socket.recv(1024).decode()
@@ -61,12 +64,14 @@ def start_smtp(receiver, title, content):
     if '220' != recv[:3]:
         print('220 reply not received from server.')
     
+    #发送helo信息，如果smtp服务器没问题，一定会返回允许信息
     helo = "HELO 666\r\n"
     smtp_socket.send(b"HELO 666\r\n")
     recv0 = smtp_socket.recv(1024).decode()  
     print("c:"+helo)
     print("s:"+recv0)
 
+    #发送登录信息，准备登录，回应为334
     auth = "AUTH LOGIN\r\n"
     smtp_socket.send(b"AUTH LOGIN\r\n")
     recv1 = smtp_socket.recv(1024).decode()  
@@ -75,7 +80,7 @@ def start_smtp(receiver, title, content):
         print('334 reply not received from server.')
     print("s:"+recv1)
 
-
+    #发送base64编码的sender邮箱，所有发送的数据都是base64编码
     smtp_socket.send(base64.b64encode(sender.encode()) + b"\r\n")
     recv2 = smtp_socket.recv(1024).decode() 
     print("c:"+sender)
@@ -83,7 +88,7 @@ def start_smtp(receiver, title, content):
         print('334 reply not received from server.')
     print("s:"+recv2)
 
-
+    #发送base64编码的授权码。
     smtp_socket.send(base64.b64encode(password.encode()) + b"\r\n")
     print("c:"+password)
     recv3 = smtp_socket.recv(1024).decode()
@@ -91,6 +96,7 @@ def start_smtp(receiver, title, content):
         print('235 reply not received from server.')
     print("s:"+recv3)
 
+    #发送邮件消息
     send_mail(smtp_socket, sender, receiver,  title, content)
 
     smtp_socket.send(b"QUIT\r\n")
@@ -125,6 +131,7 @@ def handle_request(request_data):
 
 def start_server():
 
+    #设定监听http消息的socket服务端端口
     host = '127.0.0.1'
     port = 9000
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -137,6 +144,7 @@ def start_server():
     client_socket, client_address = server_socket.accept()
     print("connect success!:",client_address)
 
+    #处理请求信息
     request_data = client_socket.recv(1024).decode('utf-8')
     handle_request(request_data)
 
@@ -144,4 +152,5 @@ def start_server():
     client_socket.sendall(response_data.encode('utf-8'))
 
 
+#开始程序
 start_server()
